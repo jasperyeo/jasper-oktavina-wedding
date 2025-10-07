a40fb97f6df22456e8fc692220e070d1e578d658
Jasper Yeo <jasper.yeo.media@gmail.com>
2025-10-07 20:59:40 +0800

    optimised gallery images

--- src/app/components/gallery/gallery.constants.ts
+++ src/app/components/gallery/gallery.constants.ts
@@ -3,62 +3,62 @@ export const GALLERY_CONTENT: any = {
     TITLE: 'galeri',
     ITEMS: [
       {
-        thumbnailImageSrc: 'prewed-id-1.jpg',
-        itemImageSrc: 'prewed-id-1.jpg',
+        thumbnailImageSrc: 'prewed-id-1.webp',
+        itemImageSrc: 'prewed-id-1.webp',
         alt: 'Prewed Shoot at Surabaya, Indonesia',
         title: 'Prewed Shoot at Surabaya, Indonesia'
       },
       {
-        thumbnailImageSrc: 'prewed-id-2.jpg',
-        itemImageSrc: 'prewed-id-2.jpg',
+        thumbnailImageSrc: 'prewed-id-2.webp',
+        itemImageSrc: 'prewed-id-2.webp',
         alt: 'Prewed Shoot at Surabaya, Indonesia',
         title: 'Prewed Shoot at Surabaya, Indonesia'
       },
       {
-        thumbnailImageSrc: 'prewed-id-3.jpg',
-        itemImageSrc: 'prewed-id-3.jpg',
+        thumbnailImageSrc: 'prewed-id-3.webp',
+        itemImageSrc: 'prewed-id-3.webp',
         alt: 'Prewed Shoot at Surabaya, Indonesia',
         title: 'Prewed Shoot at Surabaya, Indonesia'
       },
       {
-        thumbnailImageSrc: 'prewed-id-4.jpg',
-        itemImageSrc: 'prewed-id-4.jpg',
+        thumbnailImageSrc: 'prewed-id-4.webp',
+        itemImageSrc: 'prewed-id-4.webp',
         alt: 'Prewed Shoot at Surabaya, Indonesia',
         title: 'Prewed Shoot at Surabaya, Indonesia'
       },
       {
-        thumbnailImageSrc: 'prewed-id-5.jpg',
-        itemImageSrc: 'prewed-id-5.jpg',
+        thumbnailImageSrc: 'prewed-id-5.webp',
+        itemImageSrc: 'prewed-id-5.webp',
         alt: 'Prewed Shoot at Surabaya, Indonesia',
         title: 'Prewed Shoot at Surabaya, Indonesia'
       },
       {
-        thumbnailImageSrc: 'prewed-id-6.jpg',
-        itemImageSrc: 'prewed-id-6.jpg',
+        thumbnailImageSrc: 'prewed-id-6.webp',
+        itemImageSrc: 'prewed-id-6.webp',
         alt: 'Prewed Shoot at Surabaya, Indonesia',
         title: 'Prewed Shoot at Surabaya, Indonesia'
       },
       {
-        thumbnailImageSrc: 'prewed-id-7.jpg',
-        itemImageSrc: 'prewed-id-7.jpg',
+        thumbnailImageSrc: 'prewed-id-7.webp',
+        itemImageSrc: 'prewed-id-7.webp',
         alt: 'Prewed Shoot at Surabaya, Indonesia',
         title: 'Prewed Shoot at Surabaya, Indonesia'
       },
       {
-        thumbnailImageSrc: 'prewed-id-8.jpg',
-        itemImageSrc: 'prewed-id-8.jpg',
+        thumbnailImageSrc: 'prewed-id-8.webp',
+        itemImageSrc: 'prewed-id-8.webp',
         alt: 'Prewed Shoot at Surabaya, Indonesia',
         title: 'Prewed Shoot at Surabaya, Indonesia'
       },
       {
-        thumbnailImageSrc: 'prewed-id-9.jpg',
-        itemImageSrc: 'prewed-id-9.jpg',
+        thumbnailImageSrc: 'prewed-id-9.webp',
+        itemImageSrc: 'prewed-id-9.webp',
         alt: 'Prewed Shoot at Surabaya, Indonesia',
         title: 'Prewed Shoot at Surabaya, Indonesia'
       },
       {
-        thumbnailImageSrc: 'prewed-id-10.jpg',
-        itemImageSrc: 'prewed-id-10.jpg',
+        thumbnailImageSrc: 'prewed-id-10.webp',
+        itemImageSrc: 'prewed-id-10.webp',
         alt: 'Prewed Shoot at Surabaya, Indonesia',
         title: 'Prewed Shoot at Surabaya, Indonesia'
       }
